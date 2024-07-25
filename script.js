ec9b9c76a83fa411778617600e7858b05859e741
Fajla Rabby <blfajlarabby414@gmail.com>
2024-07-25 12:00:47 +0600

    Finished Project

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+let display = document.getElementById('display');
 const appendToDisplay = (num) => {
-
+    display.value += num;
 }
-
 const calculateDisplay = () => {
-
+    try {
+        display.value = eval(display.value);
+    } catch (err) {
+        display.value = err;
+    }
 }
 
 const clearDisplay = () => {
+    display.value = "";
+}
 
+const backSpace = () => {
+    let displaylastIndex = display.value.length - 1;
+    let slice = display.value.slice(0, displaylastIndex);
+    display.value = slice;
 }
